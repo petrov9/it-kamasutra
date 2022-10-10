e450e096a906402895c295036ffd1794e400f859
Anton Petrov <petrov9810@gmail.com>
2022-10-10 11:05:23 +0800

    75 lesson. redux-form введение (login)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import {Field, reduxForm} from "redux-form";
 
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+            </div>
+            <div>
+                <Field component={"input"} name={"rememberMe"} type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    );
+}
+
+const LoginReduxForm = reduxForm({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm)
 
 const Login = (props) => {
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
     return (
-        <div>login</div>
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
     );
 }
 
