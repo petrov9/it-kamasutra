25b7ee415497597866a5d59e9792d2424600deeb
Anton Petrov <petrov9810@gmail.com>
2022-10-11 17:56:59 +0800

    77 lesson. redux-form field validation

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,12 +4,18 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import {Redirect} from "react-router-dom";
 import {Field, reduxForm} from "redux-form";
+import {maxLengthCreator, required} from "../../utils/validators/validators";
+import {Textarea} from "../common/FormControls/FormsControls";
+
+let maxLength50 = maxLengthCreator(50);
 
 const AddMessageForm = (props) => {
   return (
       <form onSubmit={props.handleSubmit}>
           <div>
-            <Field component={"textarea"} name={"newMessageBody"} placeholder='Enter your message'/>
+            <Field component={Textarea} name={"newMessageBody"}
+                   validate={[required, maxLength50]}
+                   placeholder='Enter your message'/>
           </div>
           <div>
               <button>Отправить</button>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import s from './MyPosts.module.css'
 import Post from "./post/Post";
 import {Field, reduxForm} from "redux-form";
+import {maxLengthCreator, required} from "../../../utils/validators/validators";
+import {Textarea} from "../../common/FormControls/FormsControls";
+
+let maxLength10 = maxLengthCreator(10);
 
 const AddNewFormPost = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field component={"textarea"} name={"newPostText"}/>
+                <Field component={Textarea} name={"newPostText"} placeholder={"Post message"}
+                       validate={[required, maxLength10]}/>
             </div>
             <div>
                 <button>Add post</button>
